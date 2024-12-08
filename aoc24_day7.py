4c3f95671f659eeeadbfe77a8161cc787a7a71d9
Flowy <git@lwyp.fr>
2024-12-08 23:07:44 +0100

    and part2 done as well :)

--- aoc24_day7.py
+++ aoc24_day7.py
@@ -6,7 +6,6 @@ total_calibration_results = 0
 def part1(results, values):
     toreturn = 0
     for i in range(len(results)):
-        valid = False
         vals = values[i]
         num_ops = len(vals) - 1
         # we have LTR evaluation
@@ -31,16 +30,47 @@ def part1(results, values):
         #print(temporary_results)
         if results[i] in temporary_results:
             toreturn += results[i]
-
-
-
-
-        if valid:
-            toreturn += results[i]
-
     return toreturn
 
-
+def part2(results, values):
+    toreturn = 0
+    for i in range(len(results)):
+        vals = values[i]
+        num_ops = len(vals) - 1
+        # we have LTR evaluation
+        # so for each "operation" we have max 2 possible results
+        # for n operations, we have 2**n possible results
+        temporary_results = []
+        temp_result1 = int(vals[0]) + int(vals[1])
+        temp_result2 = int(vals[0]) * int(vals[1])
+        temp_result3 = int(str(vals[0]) + str(vals[1]))
+        #print("Temp result1", temp_result1)
+        #print("Temp result2", temp_result2)
+        #print("Temp result3", temp_result3)
+        temporary_results.append(temp_result1)
+        temporary_results.append(temp_result2)
+        temporary_results.append(temp_result3)
+            # 0, 2(skip first two), 7(skip 1st 2, skip 2nd 4), 15(skip 1st 2, 2nd 4, 3rd 8),
+        current_count = 0
+        if len(vals) >= 2:
+            for j in range(len(vals)-2):
+                for test_case in temporary_results[current_count:]:
+                    temp_result1 = test_case + int(vals[j+2])
+                    temp_result2 = test_case * int(vals[j+2])
+                    temp_result3 = int(str(test_case) + str(vals[j+2]))
+                    #print("Temp result1", temp_result1)
+                    #print("Temp result2", temp_result2)
+                    #print("Temp result3", temp_result3, " : ", str(test_case), " und ", str(vals[1]))
+                    temporary_results.append(temp_result1)
+                    temporary_results.append(temp_result2)
+                    temporary_results.append(temp_result3)
+                current_count += 3**(j+1)
+        #print(results[i])
+        #print(temporary_results)
+        if results[i] in temporary_results:
+            #print("FOUND")
+            toreturn += results[i]
+    return toreturn
 
 # load & process input
 with open(file) as aoc_input:
@@ -56,8 +86,9 @@ with open(file) as aoc_input:
         vals = vals.split(' ')
         values.append(vals)
     part1_result = part1(results, values)
+    part2_result = part2(results, values)
 
 
 # print results
 print("Part 1: ", part1_result)
-#print("Part 2: Reports safe with Problem Dampener: ", safe_damp_count)
+print("Part 2: ", part2_result)
