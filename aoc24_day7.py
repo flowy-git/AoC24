dfedfc4d554a04e420ab94c83899adfeb9d68913
Flowy <git@lwyp.fr>
2024-12-08 23:40:15 +0100

    tidied up a lil

--- aoc24_day7.py
+++ aoc24_day7.py
@@ -1,8 +1,19 @@
 # setup
 file = "./inputs/aoc24_day7_input.txt"
-total_calibration_results = 0
 
 
+        # we have LTR evaluation
+        # so for each "operation" we have max 2 possible results
+        # for n operations, we have 2**n possible results
+        # part1:
+            # 0, 2(skip first two), 7(skip 1st 2, skip 2nd 4), 15(skip 1st 2, 2nd 4, 3rd 8)
+        # part2:
+            # basically raises up to 3**N
+            # change accordingly
+
+
+
+# Part1, essentially keep track of 2**n vals and in the end check 
 def part1(results, values):
     toreturn = 0
     invalid_results = []
@@ -10,15 +21,12 @@ def part1(results, values):
     for i in range(len(results)):
         vals = values[i]
         num_ops = len(vals) - 1
-        # we have LTR evaluation
-        # so for each "operation" we have max 2 possible results
-        # for n operations, we have 2**n possible results
+
         temporary_results = []
         temp_result1 = int(vals[0]) + int(vals[1])
         temp_result2 = int(vals[0]) * int(vals[1])
         temporary_results.append(temp_result1)
         temporary_results.append(temp_result2)
-            # 0, 2(skip first two), 7(skip 1st 2, skip 2nd 4), 15(skip 1st 2, 2nd 4, 3rd 8),
         current_count = 0
         if len(vals) >= 2:
             for j in range(len(vals)-2):
@@ -28,8 +36,6 @@ def part1(results, values):
                     temporary_results.append(temp_result1)
                     temporary_results.append(temp_result2)
                 current_count += 2**(j+1)
-        #print(results[i])
-        #print(temporary_results)
         if results[i] in temporary_results:
             toreturn += results[i]
         else:
@@ -37,47 +43,32 @@ def part1(results, values):
             invalid_vals.append(vals)
     return invalid_results, invalid_vals, toreturn
 
+
+#  Part2, basically part1, but we extend to n**3
 def part2(results, values):
     toreturn = 0
     for i in range(len(results)):
         vals = values[i]
         num_ops = len(vals) - 1
-        # we have LTR evaluation
-        # so for each "operation" we have max 2 possible results
-        # for n operations, we have 2**n possible results
         temporary_results = []
         temp_result1 = int(vals[0]) + int(vals[1])
         temp_result2 = int(vals[0]) * int(vals[1])
         temp_result3 = int(str(vals[0]) + str(vals[1]))
-        #print("Temp result1", temp_result1)
-        #print("Temp result2", temp_result2)
-        #print("Temp result3", temp_result3)
         temporary_results.append(temp_result1)
         temporary_results.append(temp_result2)
         temporary_results.append(temp_result3)
-            # 0, 2(skip first two), 7(skip 1st 2, skip 2nd 4), 15(skip 1st 2, 2nd 4, 3rd 8),
         current_count = 0
         if len(vals) >= 2:
-            count = (len(vals)-2)*2 /3
             for j in range(len(vals)-2):
                 for test_case in temporary_results[current_count:]:
                     temp_result1 = test_case + int(vals[j+2])
                     temp_result2 = test_case * int(vals[j+2])
                     temp_result3 = int(str(test_case) + str(vals[j+2]))
-                    #print("Temp result1", temp_result1)
-                    #print("Temp result2", temp_result2)
-                    #print("Temp result3", temp_result3, " : ", str(test_case), " und ", str(vals[1]))
                     temporary_results.append(temp_result1)
                     temporary_results.append(temp_result2)
                     temporary_results.append(temp_result3)
-                current_count += 3**(j+1)
-                if j > count:
-                    continue
-                    """ if results[i] in temporary_results:
-                        break """
-                    
+                current_count += 3**(j+1)                    
         if results[i] in temporary_results:
-            #print("FOUND")
             toreturn += results[i]
     return toreturn
 
@@ -88,7 +79,6 @@ with open(file) as aoc_input:
     for line in aoc_input:
         line = line.replace("\n", "")
         line = line.replace(": ", ":")
-        #print(line)
         line = line.split(':')
         results.append(int(line[0]))
         vals = line[1]
