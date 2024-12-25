07af70f124248da736900eb8acc667a938a49957
Flowy <git@lwyp.fr>
2024-12-25 13:15:29 +0100

    yeah idk the runtime is crazy dont think this solution will ever finish - need to rework the approach

--- aoc24_day19.py
+++ aoc24_day19.py
@@ -8,12 +8,13 @@ def next_step(verifying, design, pattern, solution):
     toreturn = [0]
     solution_found = False
     print("starting step")
-    print(solution)
+    #print(solution)
     print("ver : ", verifying)
     for poss_sol in solution:
         valid = False
-        print("pos sol : ", poss_sol)
-        if poss_sol != 0 and poss_sol[0] < poss_sol[1] and poss_sol[1] < len(verifying):
+        #print("pos sol : ", poss_sol)
+        if poss_sol != 0 and poss_sol[0] <= poss_sol[1] and poss_sol[1] < len(verifying):
+            #print("check 1")
             temp_string = ""
             for i in poss_sol[2:]:
                 temp_string += i
@@ -23,18 +24,23 @@ def next_step(verifying, design, pattern, solution):
             if temp_string == verifying:
                 solution_found = True
                 print("### SOLUTION FOUND ### ", verifying, "####")
+                break
         if not solution_found and valid:
+            #print("check2")
             next_patt = []
-            next_char = verifying[poss_sol[1]+1]
+            next_three_chars = verifying[poss_sol[1]+1:poss_sol[1]+4]
+            
             for i in pattern:
-                if i[:1] == next_char:
+                cut = min(len(i), 3)
+                if i[:cut] == next_three_chars[:cut]:
                     next_patt.append(i)
             for i in next_patt:
                 new_poss_sol = copy.deepcopy(poss_sol)
                 new_poss_sol.append(i)
                 new_poss_sol[1] = new_poss_sol[1] + len(i)
                 toreturn.append(new_poss_sol)
-    print(toreturn)
+    #print(toreturn)
+    print("NEXT STEP STARTING SOON \n\n\n\n ########################################### \n\n\n\n CURRENT STEP ENDED")
     print("finished step")
     
     return toreturn, solution_found
@@ -42,20 +48,20 @@ def next_step(verifying, design, pattern, solution):
 def part1(pattern, design):
     number_possible = 0
     for check_design in design:
-        print("checking : ", check_design)
+        #print("checking : ", check_design)
         solution = [0]
         possible = False
         start = check_design[:1]
-        print("start : ", start)
+        #print("start : ", start)
         for i in pattern:
             if i[:1] == start:
                 solution.append([0, len(i) - 1, i])
         solution_found = False
         while True:
-            if(solution[0] == 1) or len(solution) == 1 or solution_found:
+            if(solution[0] == 1) or len(solution) == 1 or possible:
                 break
-            solution, solution_found = next_step(check_design, design, pattern, solution)
-        print(solution)
+            solution, possible = next_step(check_design, design, pattern, solution)
+        #print(solution)
         if(possible):
             number_possible += 1
     return number_possible
@@ -67,7 +73,7 @@ with open(file) as aoc_input:
     patterns_done = False
     for line in aoc_input:
         if line != "\n" and not patterns_done:
-            print(line)
+            #print(line)
             patterns_done = True
             line = line.replace("\n", "")
             line = line.split(", ")
@@ -76,7 +82,7 @@ with open(file) as aoc_input:
         elif line == "\n":
             continue
         line = line.replace("\n", "")
-        print(line)
+        #print(line)
         designs.append(line)
     part1 = part1(patterns, designs)
 
