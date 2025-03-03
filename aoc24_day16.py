3796ade53f803c34a7d7a10fb8efcc899dafca79
Flowy <git@lwyp.fr>
2025-03-03 10:32:59 +0100

    part1 is functional - implemented additional heuristic. still need to clean up.

--- aoc24_day16.py
+++ aoc24_day16.py
@@ -54,16 +54,16 @@ class Action:
         self.cost = cost # the cost of the action
 
 def subtask_1(problem, blacklist):
-    node = Node([1, Y-1], 0, 0, 0) # initial state -> Position X,Y = 0,0
+    node = Node([1, Y-1], Node([0, Y-1], 0, 0, 0), 0, 0) # initial state -> Position X,Y = 0,0 # added "parent" to have starting direction
     frontier = [node]
     explored_set = []
-    for i in frontier:
-        print(i.state)
+    #for i in frontier:
+    #    print(i.state)
     print("########## STARTING #############")
     while True:
-        print("///// CURRENT FRONTIER //////")
-        for i in frontier:
-            print(i.state)
+        #print("///// CURRENT FRONTIER //////")
+        #for i in frontier:
+        #    print(i.state)
         if len(frontier) == 0:
             raise ValueError("Subtask1_problem_issue_frontier_empty")
         node = frontier.pop(0)
@@ -71,9 +71,9 @@ def subtask_1(problem, blacklist):
             print("solution found")
             return solution(problem, node)
         explored_set.append(node)
-        for action in get_actions(problem, node.state, blacklist):
+        for action in get_actions(problem, node.state, blacklist, node.parent):
             #print(action)
-            child = Node(action.child_state, node, action)
+            child = Node(action.child_state, node, action, action.cost)
             if (child not in explored_set) and (child not in frontier):
                 frontier.append(child)
                 # re-sort frontier???
@@ -84,7 +84,7 @@ def subtask_1(problem, blacklist):
                     frontier[idx] = child
                 frontier.sort()
 
-def get_actions(problem, node_state, blacklist):
+def get_actions(problem, node_state, blacklist, node_parent):
     varX = node_state[0]
     varY = node_state[1]
     possible_actions = [[varX, varY+1], [varX+1, varY], [varX, varY-1], [varX-1, varY]]
@@ -94,25 +94,31 @@ def get_actions(problem, node_state, blacklist):
     for action in possible_actions:
         if action not in blacklist:
             if action[0] in ranges_x and action[1] in ranges_y:
-                actions.append(Action(action))
+                if action[0] == node_state[0] == node_parent.state[0] or action[1] == node_state[1] == node_parent.state[1]:
+                    cost = 1
+                else:
+                    cost = 1001
+                actions.append(Action(action, cost))
     return actions
 
 def solution(problem, node):
+    total_cost = 0
     start_node = Node([1, Y-1], 0, 0, 0)
     path = []
     current_node = node
     while current_node is not start_node and isinstance(current_node, Node):
+        total_cost += current_node.path_cost
         # take the node, work up towards start_node
         path.append(current_node.state)
         current_node = current_node.parent
-    return path
+    return path, total_cost
 
 def digitise_map(map):
     toreturn = []
     for idx in range(X+1):
         for idy in range(Y+1):
-            if map[idy][idx] == '#':
-                toreturn.append([idx, idy])
+            if map[idx][idy] == '#':
+                toreturn.append([idy, idx])
     return toreturn
 
 # load & process input
@@ -123,13 +129,17 @@ with open(file) as aoc_input:
         line = line.replace("\n", "")
         line = list(line)
         map.append(line)
+    print("map len", len(map))
     Y -= 1
     X -= 1
+    print("X", X)
+    print("y", Y)
     coordinates = digitise_map(map)
-    print(coordinates)
-    solution_path = part1(coordinates)
-    print(solution_path)
-    part1 = len(solution_path) - 1
+    #print(coordinates)
+    solution_path, cost = part1(coordinates)
+    #print(solution_path)
+    print(cost)
+    part1 = cost
 
 # print results
 print("Part 1: ", part1)
